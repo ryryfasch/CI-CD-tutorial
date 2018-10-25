08049c81378cef471d8407c3b2495415534ac59e
ryryfasch <ryan.fasching@colorado.edu>
2018-10-25 16:45:00 -0600

    basic nodeapp

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  // res.send('Hello World!')
+  res.render('index');
+})
+
+app.post('/', function (req, res) {
+  // res.render('index0');
+  console.log(req.body.city);
+  // res.send(req.body.city);
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
